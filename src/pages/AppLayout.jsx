2dbf07cb9f6e721106337deed302ff297bc8f2d1
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-10 23:47:48 -0300

    Create app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,11 @@
-import AppNav from "../components/AppNav";
-
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <h1>aa</h1>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
